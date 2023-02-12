853b2f8a0b17f59b12cdf14cd396a0068c6f97ef
naweasleey <nailyna1212@gmail.com>
2023-02-12 22:11:15 +0700

    Add project files.

---
+++ MyBlog/Data/MysqlConetext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBlog.Models;
+
+namespace MyBlog.Data
+{
+    public class MysqlConetext : DbContext
+    {
+        public MysqlConetext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Post> Posts { get; set; }
+    }
+}
